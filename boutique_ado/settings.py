d1a560f745a0e2fe62cabcfad9c55285123037d7
Alexandru Apostoiu <alexander.apostoiu@gmail.com>
2021-01-10 15:12:11 +0000

    returned to the initial settings

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -11,12 +11,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 import os
-from os import path
-
-if path.exists('env.py'):
-    import  env
-
-
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
